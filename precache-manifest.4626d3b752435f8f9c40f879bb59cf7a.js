3a557c78d3abf8b229f38a690941cf8fabb255b3
Ivens Signorini <ivenssignorini@mbp-de-ivens.home>
2020-02-19 21:09:37 +0100

    Updates

---
+++ precache-manifest.4626d3b752435f8f9c40f879bb59cf7a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23c3cefc35fb3e60118a1fe0e7c9649d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6925ecf781bf5a74ca2a",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "fa502d869704e16ce75f",
+    "url": "/static/js/2.e4a421a8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.e4a421a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6925ecf781bf5a74ca2a",
+    "url": "/static/js/main.20132434.chunk.js"
+  },
+  {
+    "revision": "8ad0a82b866fe80b5f42",
+    "url": "/static/js/runtime-main.8a1090fe.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
