ad703863d363e23b7979b9631f586bc2b783172c
Ivens Signorini <ivenssignorini@mbp-de-ivens.home>
2020-02-19 22:08:56 +0100

    some ...t more

---
+++ build/precache-manifest.09b3bebc431332aaad154f3c1ecc56e6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87f737e06d2c65a15a4ba1aa0a2af56c",
+    "url": "/portal/index.html"
+  },
+  {
+    "revision": "c8018edffe43f1ec5b48",
+    "url": "/portal/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "fa502d869704e16ce75f",
+    "url": "/portal/static/js/2.e4a421a8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/portal/static/js/2.e4a421a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8018edffe43f1ec5b48",
+    "url": "/portal/static/js/main.9461c3c0.chunk.js"
+  },
+  {
+    "revision": "0724734676a5b7dc3a2c",
+    "url": "/portal/static/js/runtime-main.83ada9d9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portal/static/media/logo.5d5d9eef.svg"
+  }
+]);
