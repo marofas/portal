55294fa80b68531f11fe60ba4122b0a7c229de09
Ivens Signorini <ivenssignorini@mbp-de-ivens.home>
2020-02-19 22:00:51 +0100

    some ...t

---
+++ build/precache-manifest.b39516dc5ece54de88b1defaf33076f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc2f376a90e4ca1465dc553f132b3be3",
+    "url": "/marofas/portal/index.html"
+  },
+  {
+    "revision": "3b1c0163c2ac849fa3eb",
+    "url": "/marofas/portal/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "fa502d869704e16ce75f",
+    "url": "/marofas/portal/static/js/2.e4a421a8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/marofas/portal/static/js/2.e4a421a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b1c0163c2ac849fa3eb",
+    "url": "/marofas/portal/static/js/main.3742abc1.chunk.js"
+  },
+  {
+    "revision": "a34bb9a037ff7abf58cd",
+    "url": "/marofas/portal/static/js/runtime-main.12f95bea.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/marofas/portal/static/media/logo.5d5d9eef.svg"
+  }
+]);
